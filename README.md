92391c5dd8f658766d0cd3fe27771c9f07110e4d
Peter Hunt <peter@huntfx.uk>
2023-02-12 22:32:02 +0000

    Improved syntax
    Resolves  #1.

--- README.md
+++ README.md
@@ -3,21 +3,24 @@ Easily convert units to different quantities.
 
 For conveniance, abbreviations are accepted, where an attempt will be made to calculate the type based on the other inputs (eg. "m" can evaluate as either "minute" or "metre").
 
+> **This library is deprecated**: A recommendation is to use [pint](https://github.com/hgrecco/pint) instead.
+
+
 ## Example Usage
 ```python
 >>> from unit_convert import UnitConvert
 
 # Yards + kilometres to miles
->>> UnitConvert(yards=136.23, kilometres=60).miles
-37.3597678005
+>>> UnitConvert(yards=136.23, kilometres=60)['miles']
+37.35976780046479
 
 # Bytes to terabytes
->>> UnitConvert(b=19849347813875).tb
-18.0528766704
+>>> UnitConvert('b', 19849347813875, 'megabytes', 512)['tb']
+18.053364951617368
 
-# Nanometres to metres
->>> UnitConvert(nanometres=19849347813875).m
-19849.3478139
+# List available conversions
+>>> UnitConvert(metres=1).keys()
+['nm', 'nanometres', 'μm', 'micrometres', 'mm', 'millimetres', 'cm', 'centimetres', 'i', 'inches', 'ft', 'feet', 'm', 'metres', 'meters', 'yd', 'yards', 'km', 'kilometres', 'kilometers', 'miles', 'lightyears', 'au', 'astronomical_units', 'parsec']
 ```
 
-Data, time, distance and mass are currently supported.
+Data size, time, distance and mass are supported.
--- unit_convert.py
+++ unit_convert.py
@@ -3,9 +3,10 @@
 
 from __future__ import division
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 
 from collections import defaultdict
+from copy import deepcopy
 
 
 class Unit:
@@ -230,7 +231,7 @@ class UnitConvert(object):
         },
     }
 
-    def __init__(self, **kwargs):
+    def __init__(self, *args, **kwargs):
         """Convert input kwargs into a sum of possible output values.
 
         It does this by keeping track of the possible "types" (eg. "m"
@@ -238,9 +239,30 @@ class UnitConvert(object):
         as "cm", it's obvious time is not intended.
 
         Multiple inputs of the same type will be added together.
+
+        Args:
+            Unit type followed by its size.
+            Example: UnitConvert('bytes', 52, 'mb', 1)
+
+        Kwargs:
+            Unit types and their size.
+            Example: UnitConvert(bytes=52, mb=1)
         """
         self._types = None
         self._totals = defaultdict(int)
+
+        # Convert args to kwargs
+        unit_type = None
+        for i, item in enumerate(args):
+            if i % 2:
+                if unit_type in kwargs:
+                    kwargs[unit_type] += item
+                else:
+                    kwargs[unit_type] = item
+            else:
+                unit_type = item
+
+        # Get value from kwargs (eg. {"bytes": 123, "mb": 1})
         for unit, value in kwargs.items():
             unit = unit.lower()
             if unit in self.Data:
@@ -261,16 +283,22 @@ class UnitConvert(object):
             raise ValueError('input values do not have a common type')
 
     def __getattr__(self, attr):
-        try:
-            possible_units = set(self.Data.get(attr)) & self._types
-        except TypeError:
-            raise AttributeError("'{}' object has no attribute '{}'".format(
-                self.__class__.__name__, attr
-            ))
+        """Convert to an output value.
+
+        This is left in for legacy purposes but `__getitem__` is
+        recommended instead.
+        """
+        if attr in self.Data:
+            return self.__getitem__(attr)
+        return super(UnitConvert, self).__getattribute__(attr)
+
+    def __getitem__(self, item):
+        """Convert to an output value."""
+        possible_units = set(self.Data.get(item, {})) & self._types
 
         # An invalid unit was chosen
         if not possible_units:
-            raise ValueError('unable to convert to "{}"'.format(attr))
+            raise ValueError('unable to convert to "{}"'.format(item))
 
         # There's too many types to guess
         # One example would be "m" to "m"
@@ -279,5 +307,25 @@ class UnitConvert(object):
         unit = possible_units.pop()
 
         original_value = self._totals[unit]
-        multiplier = self.Data[attr][unit]
+        multiplier = self.Data[item][unit]
         return original_value / multiplier
+
+    def keys(self):
+        """Return a list of all available unit types."""
+        return [k for k, v in self.Data.items() if self._types & set(v)]
+
+    def values(self):
+        """Return a list of all available unit values."""
+        return list(map(self.__getitem__, self.keys()))
+
+    def items(self):
+        """Return a mapping of unit types to values."""
+        keys = self.keys()
+        return dict(zip(keys, map(self.__getitem__, keys)))
+
+    def get(self, item, default=None):
+        """Get a conversion if available or fallback to the default value."""
+        try:
+            return self.__getattr__(item)
+        except ValueError:
+            return default
